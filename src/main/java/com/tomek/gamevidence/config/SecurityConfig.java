a8b961a51dc4793b8a00de4ff18ba46e972e8d25
jtomek <jan.tomek@arbes.com>
2018-10-23 01:09:53 +0200

    dalsi upravy

--- src/main/java/com/tomek/gamevidence/config/SecurityConfig.java
+++ src/main/java/com/tomek/gamevidence/config/SecurityConfig.java
@@ -1,19 +1,23 @@
 package com.tomek.gamevidence.config;
 
-//@Configuration
-public class SecurityConfig /*extends WebSecurityConfigurerAdapter*/ {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//  @Override
-//  protected void configure(HttpSecurity http) throws Exception {
-//
-//    http
-//            .csrf()
-//            .disable()
-//            .authorizeRequests()
-//            .anyRequest()
-//            .authenticated()
-//            .and()
-//            .httpBasic();
-//  }
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+
+    http
+            .csrf()
+            .disable()
+            .authorizeRequests()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .httpBasic();
+  }
 
 }
